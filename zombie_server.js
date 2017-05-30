5b821b708156e46df84abdc08437d356cfdd6112
snowback <bback0724@gmail.com>
2017-05-30 15:14:30 -0400

    modify message identifiers
    modify some messages

--- zombie_server.js
+++ zombie_server.js
@@ -6,35 +6,67 @@ var tot_user_cnt = 0;
 
 io.sockets.on('connection', function(socket) {
 	
+	let timer = setInterval(intervalFunc, 3000);
+	
 	++tot_user_cnt;
 	
-	console.log("New Client connected. : " + socket);
+	console.log("New Client connected. ( users: " + tot_user_cnt + ")");
 
 	// when user request login
-	socket.on('add user', function(user_name) {
+	socket.on('request login', function(user_name) {
 		socket.user_name = user_name;
-		console.log("add user : " + user_name);
-		socket.emit('login', {
-        numUsers: tot_user_cnt,
-      });
-	});
-	
-	socket.on('new message', function(msg) {
-		console.log('user: ' + socket.user_name + ', msg: ' + msg);
+		console.log("login new user: " + socket.user_name);
 		
-		socket.emit('new message', {
+		var x = Math.random() * (58 * 16);
+		var y = Math.random() * (40 * 16);
+		
+		socket.emit('answer login', {
 			username: socket.user_name,
-			message: msg
+			numUsers: tot_user_cnt,
+			zombiePosition: {
+				X: x,
+				Y: y
+			}
 		});
 		
-		socket.broadcast.emit('new message', {
+		socket.broadcast.emit('notify login', {
 			username: socket.user_name,
-			message: msg
+			numUsers: tot_user_cnt,
+		});
+	});
+	
+	function intervalFunc () {
+		var x = Math.random() * (58 * 16);
+		var y = Math.random() * (40 * 16);
+		console.log("x: " +  x + ", y: " + y);
+		socket.emit('notify spawn zombie', {
+			zombiePosition: {
+				X: x,
+				Y: y
+			}
+		});
+	}
+	
+	socket.on('request moving', function(position) {
+		console.log('user: ' + socket.user_name + ', x: ' + position.X + ', y: ' + position.Y);
+		
+		socket.emit('notify moving', {
+			username: socket.user_name,
+			X: position.X,
+			Y: position.Y
+		});
+		
+		socket.broadcast.emit('notify moving', {
+			username: socket.user_name,
+			X: position.X,
+			Y: position.Y
 		}); 
 	});
 	
 	// when the user disconnects.. perform this
 	socket.on('disconnect', function () {
 		console.log(socket.user_name + 'is disconnected, total_user_cnt: ' + --tot_user_cnt);
+		clearInterval(timer);
 	});
-});​
+});
+
